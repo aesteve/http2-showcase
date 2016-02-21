8efc3c1e65eac08bf28a40ee040ff6e1ba0da3e8
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-02-21 11:49:24 +0100

    first running version, but : Sending GOAWAY failed

---
+++ src/main/java/io/vertx/examples/http2/Http2ServerVerticle.java
@@ -0,0 +1,69 @@
+package io.vertx.examples.http2;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.VertxException;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.JksOptions;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class Http2ServerVerticle extends AbstractVerticle {
+
+
+    private HttpServer server;
+
+    @Override
+    public void start(Future<Void> future) {
+        server = vertx.createHttpServer(createOptions());
+        server.requestHandler(req -> req.response().end("Hello world"));
+        server.listen(res -> {
+           if (res.failed()) {
+               future.fail(res.cause());
+           } else {
+               future.complete();
+           }
+        });
+    }
+
+    @Override
+    public void stop(Future<Void> future) {
+        if (server == null) {
+            future.complete();
+            return;
+        }
+        server.close(future.completer());
+    }
+
+
+    private static HttpServerOptions createOptions() {
+        HttpServerOptions serverOptions = new HttpServerOptions()
+                .setPort(4043)
+                .setHost("localhost")
+                .setUseAlpn(true)
+                .setSsl(true)
+                .setKeyStoreOptions(getJksOptions());
+        return serverOptions;
+
+    }
+
+    private static JksOptions getJksOptions() {
+        return new JksOptions().setPath(findFileOnClasspath("tls/server-keystore.jks")).setPassword("wibble");
+    }
+
+    private static String findFileOnClasspath(String fileName) {
+        URL url = Http2ServerVerticle.class.getClassLoader().getResource(fileName);
+        if (url == null) {
+            throw new IllegalArgumentException("Cannot find file " + fileName + " on classpath");
+        }
+        try {
+            File file = new File(url.toURI());
+            return file.getAbsolutePath();
+        } catch (URISyntaxException e) {
+            throw new VertxException(e);
+        }
+    }
+}
