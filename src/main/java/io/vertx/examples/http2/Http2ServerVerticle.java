d39afa9ad0af4a0e2a12cd7e3889acf8e8cde1a8
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-02-21 21:32:50 +0100

    http 1 image example

--- src/main/java/io/vertx/examples/http2/Http2ServerVerticle.java
+++ src/main/java/io/vertx/examples/http2/Http2ServerVerticle.java
@@ -2,27 +2,57 @@ package io.vertx.examples.http2;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.VertxException;
+import io.vertx.core.MultiMap;
+import static io.vertx.core.http.HttpHeaders.*;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.net.JksOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.TemplateHandler;
+import io.vertx.ext.web.templ.HandlebarsTemplateEngine;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Http2ServerVerticle extends AbstractVerticle {
 
 
     private HttpServer server;
 
+    private final static int cols = 15;
+    private final static int rows = 15;
+    private final static int TILE_HEIGHT = 38;
+    private final static int TILE_WIDTH = 68;
+
     @Override
     public void start(Future<Void> future) {
         server = vertx.createHttpServer(createOptions());
         Router router = Router.router(vertx);
-        router.get("/assets/*").handler(StaticHandler.create("web"));
+        HandlebarsTemplateEngine engine = HandlebarsTemplateEngine.create();
+        engine.setMaxCacheSize(0);
+        router.getWithRegex(".+\\.hbs").handler(ctx -> {
+            List<List<String>> imgs = new ArrayList<>(cols);
+            for (int i = 0; i < cols; i++) {
+                List<String> rowImgs = new ArrayList<>(rows);
+                for (int j = 0; j < rows; j++) {
+                    rowImgs.add("https://localhost:4043/assets/img/stairway_to_heaven-" + i + "-" + j + ".jpeg");
+                }
+                imgs.add(rowImgs);
+            }
+            ctx.put("imgs", imgs);
+            ctx.put("tileHeight", TILE_HEIGHT);
+            ctx.put("tileWidth", TILE_WIDTH);
+            ctx.next();
+        });
+        router.getWithRegex(".+\\.hbs").handler(TemplateHandler.create(engine));
+        router.get("/assets/*").handler(ctx -> {
+           MultiMap headers = ctx.response().headers();
+            headers.add(EXPIRES, "0");
+            headers.add(CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+            ctx.next();
+        });
+        router.get("/assets/*").handler(StaticHandler.create().setCachingEnabled(false));
         server.requestHandler(router::accept);
         server.listen(res -> {
            if (res.failed()) {
@@ -47,7 +77,7 @@ public class Http2ServerVerticle extends AbstractVerticle {
         HttpServerOptions serverOptions = new HttpServerOptions()
                 .setPort(4043)
                 .setHost("localhost")
-                .setUseAlpn(true)
+                //.setUseAlpn(true)
                 .setSsl(true)
                 .setKeyStoreOptions(getJksOptions());
         return serverOptions;
