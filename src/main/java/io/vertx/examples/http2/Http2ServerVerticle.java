c3ecff724b2bdfcc874b37c1fdf9443a763d5ced
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-04-24 13:51:54 +0200

    let example as is

--- src/main/java/io/vertx/examples/http2/Http2ServerVerticle.java
+++ src/main/java/io/vertx/examples/http2/Http2ServerVerticle.java
@@ -16,86 +16,88 @@ import java.util.Random;
 
 public class Http2ServerVerticle extends AbstractVerticle {
 
-  private static final int COLS = 15;
-  private static final int ROWS = 15;
-  private static final int TILE_HEIGHT = 38;
-  private static final int TILE_WIDTH = 68;
-  private static final Random RANDOM = new Random();
+    private static final int HTTP1_PORT = 4043;
+    private static final int HTTP2_PORT = 4044;
 
-  private HttpServer http;
-  private Router router;
+    private static final int COLS = 15;
+    private static final int ROWS = 15;
+    private static final int TILE_HEIGHT = 38;
+    private static final int TILE_WIDTH = 68;
+    private static final Random RANDOM = new Random();
 
-  private static HttpServerOptions createOptions() {
-    HttpServerOptions serverOptions = new HttpServerOptions()
-        .setPort(getPort())
-        .setHost(getHost())
-        .setSsl(true)
-        .setKeyStoreOptions(getJksOptions());
-    if (isHttp2()) {
-      serverOptions.setUseAlpn(true);
+    private HttpServer http1;
+    private HttpServer http2;
+    private Router router;
+
+
+    @Override
+    public void start(Future<Void> future) {
+        createRouter();
+        http1 = vertx.createHttpServer(createOptions(false));
+        http1.requestHandler(router::accept);
+        http1.listen(res -> {
+            if (res.failed()) {
+                future.fail(res.cause());
+                return;
+            }
+            http2 = vertx.createHttpServer(createOptions(true));
+            http2.requestHandler(router::accept);
+            http2.listen(res2 -> {
+                if (res2.failed()) {
+                    future.fail(res.cause());
+                } else {
+                    future.complete();
+                }
+            });
+        });
     }
-    return serverOptions;
-  }
 
-  private static JksOptions getJksOptions() {
-    return new JksOptions().setPath("tls/server-keystore.jks").setPassword("wibble");
-  }
+    @Override
+    public void stop(Future<Void> future) {
+        http1.close(res -> http2.close(future.completer()));
+    }
 
-  private static int getPort() {
-    return Integer.valueOf(System.getenv("OPENSHIFT_DIY_PORT"));
-  }
 
-  private static String getHost() {
-    return System.getenv("OPENSHIFT_DIY_IP");
-  }
-
-  private static boolean isHttp2() {
-    return System.getProperty("http2") != null;
-  }
-
-  @Override
-  public void start(Future<Void> future) {
-    createRouter();
-    http = vertx.createHttpServer(createOptions());
-    http.requestHandler(router::accept);
-    http.listen(res -> {
-      if (res.failed()) {
-        future.fail(res.cause());
-        return;
-      }
-      future.complete();
-    });
-  }
-
-  @Override
-  public void stop(Future<Void> future) {
-    http.close(future.completer());
-  }
-
-  private void createRouter() {
-    router = Router.router(vertx);
-    HandlebarsTemplateEngine engine = HandlebarsTemplateEngine.create();
-    engine.setMaxCacheSize(0);
-    router.getWithRegex(".+\\.hbs").handler(ctx -> {
-      List<List<String>> imgs = new ArrayList<>(COLS);
-      for (int i = 0; i < COLS; i++) {
-        List<String> rowImgs = new ArrayList<>(ROWS);
-        for (int j = 0; j < ROWS; j++) {
-          rowImgs.add("/assets/img/stairway_to_heaven-" + i + "-" + j + ".jpeg?cachebuster=" + cacheBuster());
+    private static HttpServerOptions createOptions(boolean http2) {
+        HttpServerOptions serverOptions = new HttpServerOptions()
+            .setPort(http2 ? HTTP2_PORT : HTTP1_PORT)
+            .setHost("localhost")
+            .setSsl(true)
+            .setKeyStoreOptions(getJksOptions());
+        if (http2) {
+            serverOptions.setUseAlpn(true);
         }
-        imgs.add(rowImgs);
-      }
-      ctx.put("imgs", imgs);
-      ctx.put("tileHeight", TILE_HEIGHT);
-      ctx.put("tileWidth", TILE_WIDTH);
-      ctx.next();
-    });
-    router.getWithRegex(".+\\.hbs").handler(TemplateHandler.create(engine));
-    router.get("/assets/*").handler(StaticHandler.create());
-  }
+        return serverOptions;
+    }
 
-  private String cacheBuster() {
-    return Long.toString(new Date().getTime()) + RANDOM.nextLong();
-  }
+    private void createRouter() {
+        router = Router.router(vertx);
+        HandlebarsTemplateEngine engine = HandlebarsTemplateEngine.create();
+        engine.setMaxCacheSize(0);
+        router.getWithRegex(".+\\.hbs").handler(ctx -> {
+            List<List<String>> imgs = new ArrayList<>(COLS);
+            for (int i = 0; i < COLS; i++) {
+                List<String> rowImgs = new ArrayList<>(ROWS);
+                for (int j = 0; j < ROWS; j++) {
+                    rowImgs.add("/assets/img/stairway_to_heaven-" + i + "-" + j + ".jpeg?cachebuster=" + cacheBuster());
+                }
+                imgs.add(rowImgs);
+            }
+            ctx.put("imgs", imgs);
+            ctx.put("tileHeight", TILE_HEIGHT);
+            ctx.put("tileWidth", TILE_WIDTH);
+            ctx.next();
+        });
+        router.getWithRegex(".+\\.hbs").handler(TemplateHandler.create(engine));
+        router.get("/assets/*").handler(StaticHandler.create());
+    }
+
+    private String cacheBuster() {
+        return Long.toString(new Date().getTime()) + RANDOM.nextLong();
+    }
+
+    private static JksOptions getJksOptions() {
+        return new JksOptions().setPath("tls/server-keystore.jks").setPassword("wibble");
+    }
 
 }
